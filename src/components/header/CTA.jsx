4e31a17d0dcc2e887124473ce16b2497f5ea57bf
Aliking314 <appenpvtltd@gmail.com>
2024-10-04 08:41:06 +0500

    Cv File Added

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-// import CV from '../../assets/Harshit Verma_Resume.pdf'
+import CV from '../../assets/CV.pdf'
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="#" className='btn'>Download CV</a>
+      <a href={CV} download className='btn'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
